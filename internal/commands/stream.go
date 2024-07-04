13b580783434c3930a63d3874c2f0e0679e1edad
mustafa13760806 <75061017+mustafa13760806@users.noreply.github.com>
2024-07-04 11:39:36 +0330

    Update stream.go

--- internal/commands/stream.go
+++ internal/commands/stream.go
@@ -88,14 +88,14 @@ func sendLink(ctx *ext.Context, u *ext.Update) error {
 	row := tg.KeyboardButtonRow{
 		Buttons: []tg.KeyboardButtonClass{
 			&tg.KeyboardButtonURL{
-				Text: "Download",
+				Text: "دانلود",
 				URL:  link + "&d=true",
 			},
 		},
 	}
 	if strings.Contains(file.MimeType, "video") || strings.Contains(file.MimeType, "audio") || strings.Contains(file.MimeType, "pdf") {
 		row.Buttons = append(row.Buttons, &tg.KeyboardButtonURL{
-			Text: "Stream",
+			Text: "پخش آنلاین",
 			URL:  link,
 		})
 	}
