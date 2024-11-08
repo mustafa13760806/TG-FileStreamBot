a3d91b48dd129d659a06c71504b31d558753e0bd
mustafa13760806 <75061017+mustafa13760806@users.noreply.github.com>
2024-11-08 12:47:23 +0330

    Update stream.go

--- internal/commands/stream.go
+++ internal/commands/stream.go
@@ -83,19 +83,19 @@ func sendLink(ctx *ext.Context, u *ext.Update) error {
 		file.ID,
 	)
 	hash := utils.GetShortHash(fullHash)
-	link := fmt.Sprintf("%s/stream/%d?hash=%s", config.ValueOf.Host, messageID, hash)
+	link := fmt.Sprintf("%d?hash=%s", config.ValueOf.Host, messageID, hash)
 	text := []styling.StyledTextOption{styling.Code(link)}
 	row := tg.KeyboardButtonRow{
 		Buttons: []tg.KeyboardButtonClass{
 			&tg.KeyboardButtonURL{
-				Text: "Download",
+				Text: "📥 دانلود 📥",
 				URL:  link + "&d=true",
 			},
 		},
 	}
 	if strings.Contains(file.MimeType, "video") || strings.Contains(file.MimeType, "audio") || strings.Contains(file.MimeType, "pdf") {
 		row.Buttons = append(row.Buttons, &tg.KeyboardButtonURL{
-			Text: "Stream",
+			Text: "▶️ پخش آنلاین ▶️",
 			URL:  link,
 		})
 	}
